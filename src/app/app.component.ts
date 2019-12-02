b06cba63fbf72e1aaa4da093a118839e62e23829
ando-hunterz <ando.hunterz@gmail.com>
2019-12-02 23:16:55 +0700

    second comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { transition, trigger, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('1s ease-out',
+              style({ height: 300, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 300, opacity: 1 }),
+            animate('1s ease-in',
+              style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    ),
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('5s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('5s', style({ opacity: 0 }))
+      ])
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'angular-app';
