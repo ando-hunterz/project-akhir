329952674e71372ac87d79d380492e673c33cf5c
ando-hunterz <ando.hunterz@gmail.com>
2019-12-04 18:35:27 +0700

    fixed header

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,37 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { MahasiswaApiService } from "../../services/mahasiswa-api.service";
 import { Router } from "@angular/router";
-<<<<<<< HEAD
 import { xToken } from '../../model/loginDetails';
-=======
 import { Location } from "@angular/common";
->>>>>>> help
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
-<<<<<<< HEAD
   styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
-  private token: xToken = {token: null};
-=======
-  styleUrls: ["./header.component.scss"],
-
-})
-export class HeaderComponent implements OnInit {
-  private xtoken: string;
->>>>>>> help
+  private token: xToken = { token: null };
   public logStat: boolean = null;
 
   constructor(
     private mahasiswaApi: MahasiswaApiService,
-<<<<<<< HEAD
-    public route: Router
-=======
-    private route: Router,
+    public route: Router,
     private location : Location
->>>>>>> help
   ) {
     this.logStat = false;
     this.mahasiswaApi.currentToken.subscribe(value => {
@@ -50,21 +35,15 @@ export class HeaderComponent implements OnInit {
   }
 
   onClick() {
-<<<<<<< HEAD
     this.token.token = localStorage.getItem('token');
     console.log(this.token);
     if(this.token.token != null){
     console.log(this.token);
     this.mahasiswaApi.postUserVerify(this.token).subscribe(
-=======
-    console.log(this.xtoken);
-    this.mahasiswaApi.postUserVerify(this.xtoken).subscribe(
->>>>>>> help
       res => {
         this.route.navigate(["/homepage"]);
       },
       err => {
-<<<<<<< HEAD
         console.log(err);
         this.logStat = false;
         this.route.navigate([""]);
@@ -75,12 +54,6 @@ export class HeaderComponent implements OnInit {
       this.logStat = false;
       this.route.navigate([""]);
     }
-=======
-        alert(err);
-        this.route.navigate([""]);
-      }
-    );
->>>>>>> help
   }
 
   logIn() {
@@ -88,20 +61,13 @@ export class HeaderComponent implements OnInit {
   }
 
   logOut() {
-<<<<<<< HEAD
-    this.token = null;
-=======
-    this.xtoken = null;
->>>>>>> help
+    this.token.token = null;
     this.logStat = false;
     localStorage.removeItem("token");
     this.route.navigate(["login"]);
   }
-<<<<<<< HEAD
-=======
 
   cancel() {
     this.location.back();
   }
->>>>>>> help
 }
