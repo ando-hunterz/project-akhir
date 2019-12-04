e19885c7b86c20b603caf7618895a14c32ca5d42
ando-hunterz <ando.hunterz@gmail.com>
2019-12-04 17:05:46 +0700

    initiate

---
+++ src/app/shared/component/header/header.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from "@angular/core";
+import { MahasiswaApiService } from "../../services/mahasiswa-api.service";
+import { Router } from "@angular/router";
+import { xToken } from '../../model/loginDetails';
+
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
+})
+export class HeaderComponent implements OnInit {
+  private token: xToken = {token: null};
+  public logStat: boolean = null;
+
+  constructor(
+    private mahasiswaApi: MahasiswaApiService,
+    public route: Router
+  ) {
+    this.logStat = false;
+    this.mahasiswaApi.currentToken.subscribe(value => {
+      console.log(value);
+      if (value == null) {
+        this.logStat = false;
+      } else {
+        this.logStat = true;
+      }
+    },
+      err => { console.log(err); });
+  }
+
+  ngOnInit() {
+    this.logStat = false;
+  }
+
+  onClick() {
+    this.token.token = localStorage.getItem('token');
+    console.log(this.token);
+    if(this.token.token != null){
+    console.log(this.token);
+    this.mahasiswaApi.postUserVerify(this.token).subscribe(
+      res => {
+        this.route.navigate(["/homepage"]);
+      },
+      err => {
+        console.log(err);
+        this.logStat = false;
+        this.route.navigate([""]);
+      }
+    );
+    }
+    else{
+      this.logStat = false;
+      this.route.navigate([""]);
+    }
+  }
+
+  logIn() {
+    this.route.navigate(["login"]);
+  }
+
+  logOut() {
+    this.token = null;
+    this.logStat = false;
+    localStorage.removeItem("token");
+    this.route.navigate(["login"]);
+  }
+}
