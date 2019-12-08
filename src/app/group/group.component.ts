41a15ad402c1e5d747a277bb5310329771c52ee6
ando-hunterz <ando.hunterz@gmail.com>
2019-12-08 20:27:43 +0700

    fixed group.ts

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,33 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Mahasiswa } from '../shared/model/mahasiswa';
-import { MahasiswaApiService } from '../shared/services/mahasiswa-api.service';
-
-@Component({
-  selector: 'app-group',
-  templateUrl: './group.component.html',
-  styleUrls: ['./group.component.scss']
-})
-export class GroupComponent implements OnInit {
-
-  public mahasiswa: Mahasiswa = null;
-
-  constructor(
-    private mahasiswaApi: MahasiswaApiService) { }
-
-  ngOnInit() {
-    this.mahasiswaApi.getAllMahasiswaData().subscribe(
-      result => {
-        this.mahasiswa = result;
-        console.log(result);
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
-
-}
-import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import daftar from '../shared/model/daftar.json';
 import { SlicePipe } from '@angular/common';
